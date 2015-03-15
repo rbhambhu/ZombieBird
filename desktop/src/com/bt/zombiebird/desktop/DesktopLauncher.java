0785818b55c540a5421af9cd581e4107963f2757
Xavier Byrd <xavierbyrd@live.com>
2015-03-15 01:39:37 -0700

    Scaffolding.
    Basic setup for android studio done. This test works on my Android Nexus
    4 phone.

---
+++ desktop/src/com/bt/zombiebird/desktop/DesktopLauncher.java
@@ -0,0 +1,12 @@
+package com.bt.zombiebird.desktop;
+
+import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
+import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
+import com.bt.zombiebird.ZBGame;
+
+public class DesktopLauncher {
+	public static void main (String[] arg) {
+		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
+		new LwjglApplication(new ZBGame(), config);
+	}
+}
